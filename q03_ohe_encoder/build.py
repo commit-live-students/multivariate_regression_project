7bc19f386aca0194c900e4925b5f47ffa2f30a74
vp999 <patilvish@gmail.com>
2018-02-19 14:42:16 +0000

    Done

--- q03_ohe_encoder/build.py
+++ q03_ohe_encoder/build.py
@@ -5,15 +5,22 @@ import pandas as pd
 import numpy as np
 
 df = load_data('data/student-mat.csv')
- 
+
 x_train, x_test, y_train, y_test =  split_dataset(df)
 
 category_index = [x for x in range(len(df.columns)) if df[df.columns[x]].dtype == 'object']
 
 
-def ohe_encode(X,X_test,category_index=category_index):
+def ohe_encode(X_train,X_test,category_index=category_index):
+    X_cat= X_train.iloc[:,category_index]
+    X_train=pd.concat([X_train,pd.get_dummies(X_cat,columns=X_cat.columns )], axis=1);
+    X_train=X_train.drop(X_cat.columns, axis=1)
 
-    
+    X_cat_test= X_test.iloc[:,category_index]
+    X_test=pd.concat([X_test,pd.get_dummies(X_cat_test,columns=X_cat_test.columns )], axis=1)
+    X_test=X_test.drop(X_cat_test.columns, axis=1)
+    return X_train, X_test
 
-    
-   
+#X_train, X_test= ohe_encode(x_train,x_test,category_index=category_index)
+#print ( X_train.columns)
+#print (x_train.head(2))
