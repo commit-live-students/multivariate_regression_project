7d69229e88efe4c40e176e128da835e655d220be
TheSarang <narkhedesarang@gmail.com>
2018-02-20 15:27:08 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,10 @@
+# %load q01_load_data/build.py
 import pandas as pd
 
-#path = '../data/student-mat.csv'
+path = './data/student-mat.csv'
 def load_data(path):
-    
+    df = pd.read_table(path,sep=';')
+    return df
+
+
+
