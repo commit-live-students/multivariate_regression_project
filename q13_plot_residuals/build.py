563f952613d88317682604c868996e64231c5617
vp999 <patilvish@gmail.com>
2018-02-21 04:57:29 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -1,6 +1,11 @@
-
-
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 def plot_residuals(y_test,y_pred,name):
-    
+    error_residuals = y_test-y_pred
+    plt.scatter(y_test,error_residuals)
+    plt.title(name)
+    plt.xlabel("Actual Values")
+    plt.ylabel("Residual Error of predicted values")
+    plt.show()
