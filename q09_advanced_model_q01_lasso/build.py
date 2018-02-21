253929a51c6ecd3217e1606537c4610380ffeb04
vp999 <patilvish@gmail.com>
2018-02-21 02:22:30 +0000

    Done

--- q09_advanced_model_q01_lasso/build.py
+++ q09_advanced_model_q01_lasso/build.py
@@ -19,5 +19,33 @@ x_train, x_test, y_train, y_test =  split_dataset(df)
 x_train,x_test = label_encode(x_train,x_test)
 
 # Write your solution here
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+def create_model(model,x_train, x_test, y_train, y_test):
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
+
+    kfold = KFold(n_splits=3, random_state=7)
+    cross_validation = cross_val_score(estimator=model, X=x_train, y=y_train, cv=kfold, scoring=('r2')).mean()
+
+    mse= mean_squared_error(y_test, y_pred )
+    mae= mean_absolute_error (y_test,y_pred)
+    r2= r2_score(y_test,y_pred)
+
+    results =  pd.DataFrame()
+    results["score"]= pd.Series([cross_validation,mse,mae,r2])
+    results.index = ['cross_validation','rmse','mae','r2']
+
+
+    scores = pd.DataFrame([[cross_validation,mae,r2,np.sqrt( mse)]])
+    return model, y_pred, scores
+
 def lasso(x_train, x_test, y_train, y_test,alpha=0.1):
-    
+    model = Lasso(alpha=alpha , random_state=7)
+    return create_model(model,x_train, x_test, y_train, y_test)
+
+print ( 'scores            : cross_validation     mae      r2    rmse')
+_,_,scores=lasso(x_train, x_test, y_train, y_test)
+print ( 'lasso             :    %f      %f %f %f '%(  scores[0],scores[1],scores[2],scores[3]))
