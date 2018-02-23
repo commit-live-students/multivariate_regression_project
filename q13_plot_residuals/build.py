8f1e15997c3e821d51b9ca516e9f0b7d8f7ec07f
djocz <jocelynd789@gmail.com>
2018-02-23 12:35:45 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -1,6 +1,8 @@
 
 
 import matplotlib.pyplot as plt
-
+#%matplotlib inline
 def plot_residuals(y_test,y_pred,name):
-    
+    residuals = y_test - y_pred
+    plt.scatter(y_test,residuals)
+    plt.title(name)
