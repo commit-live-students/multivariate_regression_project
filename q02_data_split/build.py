c55fd711e5c1f4e21084e5f63d968113301d0872
chiragpujari <chg.p786@gmail.com>
2018-02-23 05:29:54 +0000

    Done

--- q02_data_split/build.py
+++ q02_data_split/build.py
@@ -3,4 +3,6 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 df = load_data('data/student-mat.csv')
 def split_dataset(df):
-    
+    X=df.iloc[:,:-1]
+    y=df.G3
+    return train_test_split(X, y,test_size=0.2)
