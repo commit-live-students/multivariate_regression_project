101409ac12fec0a43aaa3ca1c58c65f77d82f307
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-28 14:16:19 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -1,3 +1,4 @@
+# %load q06_cross_validation/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -10,12 +11,15 @@ from sklearn.model_selection import KFold
 import numpy as np
 
 df = load_data('data/student-mat.csv')
- 
+
 x_train, x_test, y_train, y_test =  split_dataset(df)
 
 x_train,x_test = label_encode(x_train,x_test)
 
 model =linear_regression(x_train,y_train)
 
-def cross_validation_regressor(model,X,y):
-    
+def cross_validation_regressor(model,x_train,y_train):
+    scores = cross_val_score(model, x_train, y_train)
+    return float(scores.mean())
+
+cross_validation_regressor(model,x_train,y_train)
