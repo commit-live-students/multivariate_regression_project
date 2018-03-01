fc223eafb052ef3c926130009e8ef240b660f014
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-03-01 03:07:09 +0000

    Done

--- q11_feature_selection_q02_best_k_features/build.py
+++ q11_feature_selection_q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q11_feature_selection_q02_best_k_features/build.py
 # Default imports
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
@@ -19,9 +20,17 @@ x_train,x_test = label_encode(x_train,x_test)
 
 
 np.random.seed(9)
-def percentile_k_features(X,y, k=50):
-    
+def percentile_k_features(x_train,y_train, k=50):
+    model = f_regression
 
+    skb = SelectPercentile(model, k)
+    predictors = skb.fit_transform(x_train, y_train)
+    scores = list(skb.scores_)
+    #print scores
+    top_k_index = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:predictors.shape[1]]
+    #print top_k_index
+    top_k_predictors = [x_train.columns[i] for i in top_k_index]
 
+    return top_k_predictors
 
-
+percentile_k_features(x_train,y_train,50)
