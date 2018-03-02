22ba974f99f66b6c5c19b5c59d60c978176b160e
cchopade <cchopade@gmail.com>
2018-03-02 04:56:23 +0000

    Done

--- q02_data_split/build.py
+++ q02_data_split/build.py
@@ -2,5 +2,11 @@ from greyatomlib.multivariate_regression_project.q01_load_data.build import load
 from sklearn.model_selection import train_test_split
 import pandas as pd
 df = load_data('data/student-mat.csv')
+
 def split_dataset(df):
-    
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+
+    X_train,X_test,y_train,y_test = train_test_split(X,y,train_size = 0.8, random_state = 7)
+
+    return X_train, X_test, y_train, y_test
