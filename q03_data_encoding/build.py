6f83f77c718b79e08e15f32627509089e0c5b5ed
bhushan45 <bhushanshewale45@gmail.com>
2018-03-04 11:00:59 +0000

    Done

--- q03_data_encoding/build.py
+++ q03_data_encoding/build.py
@@ -4,11 +4,12 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
 df = load_data('data/student-mat.csv')
- 
+
 x_train, x_test, y_train, y_test =  split_dataset(df)
 
 def label_encode(X_train,X_test):
     """encodes the non-numeric values to numeric"""
-    
-
-
+    Columns = X_train.select_dtypes(exclude=[np.number]).columns
+    X_train.loc[:,Columns]=X_train[Columns].apply( LabelEncoder().fit_transform)
+    X_test.loc[:,Columns]=X_test[Columns].apply( LabelEncoder().fit_transform)
+    return X_train, X_test
