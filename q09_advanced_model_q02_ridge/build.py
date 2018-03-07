e8104dda072cd99522945dec0ddfe8cd90124c4d
ms10031990 <ms10031990@yahoo.com>
2018-03-07 18:19:17 +0000

    Done

--- q09_advanced_model_q02_ridge/build.py
+++ q09_advanced_model_q02_ridge/build.py
@@ -13,11 +13,17 @@ from greyatomlib.multivariate_regression_project.q06_cross_validation.build impo
 np.random.seed(9)
 
 df = load_data('data/student-mat.csv')
- 
+
 x_train, x_test, y_train, y_test =  split_dataset(df)
 
 x_train,x_test = label_encode(x_train,x_test)
 
 def ridge(x_train, x_test, y_train, y_test,alpha=0.1):
-    
+    clf = Ridge(alpha=alpha,normalize = True, random_state= 9)
+    clf.fit(x_train, y_train)
+    val = cross_validation_regressor(clf,x_train,y_train)
+    y_pred, mse, mae, r2 = regression_predictor(clf, x_test, y_test)
+    temp_list = [val, mae, r2, np.sqrt(mse)]
+    stat = pd.DataFrame([temp_list])
 
+    return clf, y_pred, stat
