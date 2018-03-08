c3d6c667c66cd27d1923c5e0b0dc608f8d959c55
ms10031990 <ms10031990@yahoo.com>
2018-03-08 16:08:38 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -3,4 +3,8 @@
 import matplotlib.pyplot as plt
 
 def plot_residuals(y_test,y_pred,name):
-    
+    plt.scatter(y_test, y_test-y_pred)
+    plt.title(name)
+    plt.xlabel('Actual Y')
+    plt.ylabel('Residual Error')
+    plt.show()
