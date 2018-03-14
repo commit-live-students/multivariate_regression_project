74886d9f823671f34eb59857efc7e28aeceba57c
Bijit Deka <bijitdeka23@gmail.com>
2018-03-14 20:17:20 +0530

    Update build.py

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -2,7 +2,4 @@ import pandas as pd
 
 #path = '../data/student-mat.csv'
 def load_data(path):
-    dataframe = pd.read_table(path, sep=';')
-    dataframe = dataframe.sample(frac=1, random_state=7).reset_index(drop=True)
-    return dataframe
     
