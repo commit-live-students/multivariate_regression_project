f51da52a8b5eb096afae0e778f30df11488b1aab
Bijit Deka <bijitdeka23@gmail.com>
2018-03-14 20:23:55 +0530

    Update build.py

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -2,14 +2,4 @@
 
 import matplotlib.pyplot as plt
 
-def plot_residuals(y_test,y_pred,name):
-    error_residuals = y_test - y_pred
-    plt.figure(figsize=(15, 8))
-    plt.scatter(y_test, error_residuals)
-    plt.title('Residual plot')
-    plt.xlabel('Grade')
-    plt.ylabel('Errors')
-    plt.savefig(name)
-    plt.show()
-
-    
+# Write your code below
