f00fca06a9ff2885cdea3d3e8010f16d78ac658c
viishnu712 <viishnu712@gmail.com>
2018-03-20 09:36:33 +0000

    Done

--- q02_data_split/build.py
+++ q02_data_split/build.py
@@ -4,5 +4,9 @@ import pandas as pd
 df = load_data('data/student-mat.csv')
 
 # Write your code below
-    
-    
+
+def split_dataset(df):
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=9)
+    return  X_train,X_test,y_train,y_test
