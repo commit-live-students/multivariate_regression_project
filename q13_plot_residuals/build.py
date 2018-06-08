a9892a12949ecc535da023212e728568fb52a919
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-06-08 15:27:01 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -1,5 +1,10 @@
-
-
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # Write your code below
+def plot_residuals(y_actual,y_pred,name):
+    plt.plot(y_actual,y_pred)
+    plt.show()
+
+#plot_residuals([1,2,3,4],[4,5,6,7],'hello')
