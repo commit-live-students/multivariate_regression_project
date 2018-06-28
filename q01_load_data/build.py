ed27b3dda20b24cd528b08690075352683966c1b
navazsayani <navaz.sayani@somaiya.edu>
2018-06-28 07:41:17 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,4 +1,11 @@
+# %load q01_load_data/build.py
 import pandas as pd
-
+import numpy as np
 # Write your code below
-    
+def load_data(path):
+    np.random.seed(7)
+    df = pd.read_table(path, sep=';')
+    return df
+
+
+
