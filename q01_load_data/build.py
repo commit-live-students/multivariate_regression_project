dc66430ed331f70f38df37cf992f8f7534cd734f
sonal7930 <sonal.chandra7930@gmail.com>
2018-07-07 23:51:26 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,4 +1,9 @@
+# %load q01_load_data/build.py
 import pandas as pd
 
-# Write your code below
-    
+path = 'data/student-mat.csv'
+def load_data(path):
+    dataframe = pd.read_table(path, sep=';')
+    dataframe = dataframe.sample(frac=1, random_state=7).reset_index(drop=True)
+    return dataframe
+
