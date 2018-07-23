fb98fb3d8aaf4f1d85d81470561b4ca7cf9659df
siddesh001 <siddesh.001@gmail.com>
2018-07-23 09:47:46 +0000

    Done

--- q05_linear_regression_model/build.py
+++ q05_linear_regression_model/build.py
@@ -1,3 +1,4 @@
+# %load q05_linear_regression_model/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -12,4 +13,10 @@ x_train, x_test = label_encode(x_train,x_test)
 
 
 # Write your code below
+def linear_regression(X,y):
+    return LinearRegression(X,y)
+     
     
+    
+
+
