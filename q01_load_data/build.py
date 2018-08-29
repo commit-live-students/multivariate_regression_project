e256e2de33e43caf8c01a1fa6bff4baeb8302159
HardikaB <hardi.2607@gmail.com>
2018-08-29 17:35:52 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,4 +1,16 @@
+# %load q01_load_data/build.py
 import pandas as pd
+from sklearn.utils import shuffle
+
 
 # Write your code below
+def load_data(path):
+    df=pd.read_table(path,sep=';')
+    df = shuffle(df)
+    return df
+
+#path='data/student-mat.csv'    
+#load_data(path)
     
+
+
