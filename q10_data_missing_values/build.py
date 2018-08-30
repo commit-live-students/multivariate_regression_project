c69019822d430dfe76db9c10b93927952cf90f72
HardikaB <hardi.2607@gmail.com>
2018-08-30 18:17:56 +0000

    Done

--- q10_data_missing_values/build.py
+++ q10_data_missing_values/build.py
@@ -1,3 +1,4 @@
+# %load q10_data_missing_values/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 from greyatomlib.multivariate_regression_project.q03_data_encoding.build import label_encode
@@ -10,4 +11,13 @@ x_train, x_test, y_train, y_test =  split_dataset(df)
 x_train,x_test = label_encode(x_train,x_test)
 
 # Write your code below
-    
+def describe_df(x_train):
+    #print(x_train.shape)
+    descD=x_train.describe()
+    #x_train.info()
+    #abc=x_train.columns.apply(pd.value_counts(x_train[x]))
+    #print(x_train['absences'].value_counts())
+    vc=x_train.apply(pd.value_counts)
+    return descD,vc
+#describe_df(x_train)
+
