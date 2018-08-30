27c4d80ab97b7c39a38ae0a4d116def8c414aa38
HardikaB <hardi.2607@gmail.com>
2018-08-30 01:13:41 +0000

    Done

--- q07_regression_pred/build.py
+++ q07_regression_pred/build.py
@@ -1,3 +1,4 @@
+# %load q07_regression_pred/build.py
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
@@ -19,4 +20,18 @@ model =linear_regression(x_train,y_train)
 val = cross_validation_regressor(model,x_train,y_train)
 
 
-# Write your code below    
+# Write your code below
+def regression_predictor(model, x_test, y_test):
+    y_pred=model.predict(x_test)
+    mse=mean_squared_error(y_test,y_pred)
+    mae=mean_absolute_error(y_test,y_pred)
+    r2=r2_score(y_test,y_pred)
+    return y_pred, mse, mae, r2
+
+
+
+
+#y_pred, mse, mae, r2 = regression_predictor(model, x_test, y_test)
+
+
+
