1c19775d9f4efc81ebbbf43855bb826ec802ab5d
abkedar <anupbhande@gmail.com>
2018-09-03 16:00:31 +0000

    Done

--- q06_cross_validation/build.py
+++ q06_cross_validation/build.py
@@ -1,3 +1,4 @@
+# %load q06_cross_validation/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -11,11 +12,21 @@ import numpy as np
 
 df = load_data('data/student-mat.csv')
  
-x_train, x_test, y_train, y_test =  split_dataset(df)
+x_train, x_test, y_train, y_test =  split_dataset(df)   
 
 x_train,x_test = label_encode(x_train,x_test)
 
 model =linear_regression(x_train,y_train)
 
 # Write your code below
-    
+def cross_validation_regressor(model, x, y):
+    #kf = RepeatedKFold(n_splits = 5, n_repeats=10, random_state=None)
+    #for train_index, test_index in kf.split(x):
+        #X_train, X_test = X[train_index], X[test_index]
+        #y_train, y_test = y[train_index], y[test_index]
+        
+    r2_score = cross_val_score(model, x, y, cv = 3)
+    return (r2_score.mean())
+cross_validation_regressor(model, x_train, y_train)    
+
+
