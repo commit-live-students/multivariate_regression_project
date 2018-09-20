c8f82c9d40484175f5fd8b52d1b6fcdf7b6b3470
abkedar <anupbhande@gmail.com>
2018-09-20 10:19:06 +0000

    Done

--- q04_data_visualisation/build.py
+++ q04_data_visualisation/build.py
@@ -1,4 +1,3 @@
-# %load q04_data_visualisation/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 from greyatomlib.multivariate_regression_project.q03_data_encoding.build import label_encode
@@ -6,18 +5,19 @@ from greyatomlib.multivariate_regression_project.q03_data_encoding.build import 
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas.plotting import scatter_matrix
+plt.switch_backend('agg')
 data = load_data('data/student-mat.csv') 
+
 x_train, x_test, y_train, y_test =  split_dataset(data)
 x_train,x_test = label_encode(x_train,x_test)
+train_1 = pd.concat([x_train, y_train], axis = 1)
 
 # Write your code below
-def visualise_data(df):
-    data_1 = df
-    train_1 = pd.concat([x_train, y_train], axis = 1)
-    (scatter_matrix(train_1, alpha=0.2, diagonal='hist'))
+def visualise_data(data,figname):
+  
+    
+    scatter_matrix(data, alpha=0.2, diagonal='kde')
     plt.show()
-    #return(axs)
 
-visualise_data(data)
 
 
