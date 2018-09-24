a8ff22279121acc75e5e2f22088b711fa38fc2db
sonik8494 <sourabh.niks1994@gmail.com>
2018-09-24 06:41:33 +0000

    Done

--- q03_data_encoding/build.py
+++ q03_data_encoding/build.py
@@ -1,3 +1,4 @@
+# %load q03_data_encoding/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 from sklearn.preprocessing import LabelEncoder
@@ -8,7 +9,13 @@ df = load_data('data/student-mat.csv')
 x_train, x_test, y_train, y_test =  split_dataset(df)
 
 # Write your code below
-    
-    
+def label_encode(x_train,x_test):
+    labelencoder = LabelEncoder()
+
+
+    X_transfrom = x_train.apply(labelencoder.fit_transform)
+    X_test_transform = x_test.apply(labelencoder.fit_transform)
+    return X_transfrom,X_test_transform
+label_encode(x_train,x_test)
 
 
