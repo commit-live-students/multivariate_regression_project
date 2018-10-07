2911fe0862398fa4fc579701aeb718fc5f475832
sreelekhaWoonna <sreelekha4877@gmail.com>
2018-10-07 18:38:14 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,4 +1,12 @@
+# %load q01_load_data/build.py
 import pandas as pd
-
+path = 'data/student-mat.csv'
 # Write your code below
-    
+def load_data(path):
+    df =pd.read_csv(path,sep=';')
+    df.sample(frac=1).reset_index(drop=True)
+    return df   
+
+
+
+
