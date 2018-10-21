a858ccc4f91b6ad640235a438a5965a41bdc8ad1
rushabh-mehta <rushabh123@gmail.com>
2018-10-21 08:02:53 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,4 +1,10 @@
+# %load q01_load_data/build.py
 import pandas as pd
 
 # Write your code below
-    
+def load_data(path):
+    df = pd.read_csv(path,sep=';')
+    df = df.sample(n=df.shape[0],random_state=7)
+    return df
+
+
