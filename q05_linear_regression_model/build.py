bea7cbab535ddd40ab59cde584029a9392b02144
rajeshbrid <rajesh_brid@hotmail.com>
2018-10-25 03:41:10 +0000

    Done

--- q05_linear_regression_model/build.py
+++ q05_linear_regression_model/build.py
@@ -1,3 +1,4 @@
+# %load q05_linear_regression_model/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -7,9 +8,16 @@ from greyatomlib.multivariate_regression_project.q03_data_encoding.build import 
 df = load_data('data/student-mat.csv')
 
 x_train, x_test, y_train, y_test =  split_dataset(df)
-
 x_train, x_test = label_encode(x_train,x_test)
 
-
 # Write your code below
-    
+
+def linear_regression(X,y):
+    regressor = LinearRegression()
+    #print (regressor.fit(X,y))
+    return regressor.fit(X,y)
+
+#linear_regression(X,y)
+
+
+
