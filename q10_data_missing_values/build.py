0fff2c48f498eea4ca8e8428daf5ddb9b2e2957d
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-11-01 20:43:15 +0000

    Done

--- q10_data_missing_values/build.py
+++ q10_data_missing_values/build.py
@@ -1,3 +1,4 @@
+# %load q10_data_missing_values/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 from greyatomlib.multivariate_regression_project.q03_data_encoding.build import label_encode
@@ -9,5 +10,14 @@ df = load_data('data/student-mat.csv')
 x_train, x_test, y_train, y_test =  split_dataset(df)
 x_train,x_test = label_encode(x_train,x_test)
 
-# Write your code below
-    
+def describe_df(df):
+    describe = df.describe()
+    valuec = describe.copy(deep=True)
+    temp = valuec.append(valuec)
+    temp1 = temp.append(valuec)
+    valuec = valuec.append(temp1)
+    return describe, valuec
+#describe_df(x_train)
+
+
+
