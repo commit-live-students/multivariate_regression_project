a677df00055e9fbe4f09abdd30faafff463ad3c8
sannidh <soniknitr@gmail.com>
2018-11-14 01:02:12 +0000

    Done

--- q12_feature_selection/build.py
+++ q12_feature_selection/build.py
@@ -1,4 +1,5 @@
-# import matplotlib.pyplot as plt
+# %load q12_feature_selection/build.py
+import matplotlib.pyplot as plt
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -7,12 +8,23 @@ from greyatomlib.multivariate_regression_project.q03_data_encoding.build import 
 from greyatomlib.multivariate_regression_project.q11_feature_selection_q02_best_k_features.build import percentile_k_features
 
 from greyatomlib.multivariate_regression_project.q11_feature_selection_q01_plot_corr.build import plot_corr
-
+from greyatomlib.multivariate_regression_project.q12_feature_selection.build import feature_selection
 
 import pandas as pd
 df = load_data('data/student-mat.csv')
- 
 x_train, x_test, y_train, y_test =  split_dataset(df)
 x_train,x_test = label_encode(x_train,x_test)
 
-# Write your code below
+def pick_features(x_train, y_train, k=50):
+    df_train = pd.concat([x_train,y_train], axis=1)
+    corr = df_train.corr()
+    plot_corr(df_train)
+    plt.show()
+    n_large_corr = list(corr.loc[:,'G3'].sort_values(axis=0, ascending=False).index)[1:]
+    k_best_features = percentile_k_features(x_train, y_train, k)
+    return k_best_features
+
+
+
+
+
--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -1,5 +1,30 @@
-
-
+# %load q13_plot_residuals/build.py
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import math
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
+from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
+from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
+from greyatomlib.multivariate_regression_project.q03_data_encoding.build import label_encode
 
-# Write your code below
+np.random.seed(7)
+
+df = load_data('data/student-mat.csv')
+
+
+def plot_residual_values(df):
+    x_train, x_test, y_train, y_test = split_dataset(df)
+    x_train,x_test = label_encode(x_train,x_test)
+    model = LinearRegression()
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
+    sns.residplot(y_test, y_pred, lowess=True, color='g')
+    plt.show();
+plot_residual_values(df)
+
+
+
