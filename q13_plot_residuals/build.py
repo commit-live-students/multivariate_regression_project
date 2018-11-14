c996ef1f7e49ae189b4a514bfa52771e1dc01b03
sannidh <soniknitr@gmail.com>
2018-11-14 15:08:13 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -14,17 +14,23 @@ from greyatomlib.multivariate_regression_project.q03_data_encoding.build import 
 np.random.seed(7)
 
 df = load_data('data/student-mat.csv')
+plt.switch_backend('agg')
 
-
-def plot_residual_values(df):
-    x_train, x_test, y_train, y_test = split_dataset(df)
-    x_train,x_test = label_encode(x_train,x_test)
-    model = LinearRegression()
-    model.fit(x_train,y_train)
-    y_pred = model.predict(x_test)
-    sns.residplot(y_test, y_pred, lowess=True, color='g')
+x_train, x_test, y_train, y_test = split_dataset(df)
+x_train,x_test = label_encode(x_train,x_test)
+model = LinearRegression()
+model.fit(x_train,y_train)
+y_pred = model.predict(x_test)
+def plot_residuals(y_test, y_pred, name):
+    error_residuals = y_test - y_pred
+    plt.figure(figsize=(15,8))
+    plt.scatter(y_test, error_residuals)
+    plt.title('Residual plot')
+    plt.xlabel('Grade')
+    plt.ylabel('Residuals')
+    plt.savefig(name)
     plt.show();
-plot_residual_values(df)
+plot_residuals(y_test, y_pred,'./images/res.png')
 
 
 
