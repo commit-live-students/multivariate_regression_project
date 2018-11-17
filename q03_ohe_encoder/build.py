96726e4fedd1dd1e8096a9d7974ec615b34ebe30
prodigy803 <pushkaraj803@gmail.com>
2018-11-17 10:51:49 +0000

    Done

--- q03_ohe_encoder/build.py
+++ q03_ohe_encoder/build.py
@@ -1,6 +1,7 @@
+# %load q03_ohe_encoder/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
 import pandas as pd
 import numpy as np
 
@@ -12,8 +13,14 @@ category_index = [x for x in range(len(df.columns)) if df[df.columns[x]].dtype =
 
 
 # Write your code below
+def ohe_encode(X,X_test,category_index=([0, 1, 3, 4, 5, 8, 9, 10, 11, 15, 16, 17, 18, 19, 20, 21, 22])):
+    ohe_enc = OneHotEncoder()
+    label_enc = LabelEncoder()
+    for i in range(len(category_index)):
+        X.iloc[:,category_index[i]] = label_enc.fit_transform(X.iloc[:,category_index[i]])
+        X_test.iloc[:,category_index[i]] = label_enc.fit_transform(X_test.iloc[:,category_index[i]])
 
-    
+    return ohe_enc.fit_transform(X),ohe_enc.fit_transform(X_test)
 
-    
-   
+
+
