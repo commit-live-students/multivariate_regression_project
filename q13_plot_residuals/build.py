1ad06b57da660f1d90d605052123e8c9c5c59250
prodigy803 <pushkaraj803@gmail.com>
2018-11-28 00:58:17 +0000

    Done

--- q13_plot_residuals/build.py
+++ q13_plot_residuals/build.py
@@ -1,5 +1,11 @@
+# %load q13_plot_residuals/build.py
 
 
 import matplotlib.pyplot as plt
-
 # Write your code below
+def plot_residuals(y_test,y_pred,name):
+    plt.scatter(y_test,y_pred)
+    plt.title('name')
+    plt.show()
+
+
