48a9f82795da93dd317e94a46fa8ba1a773fb0ae
Abhimanyu22 <link2manyu@hotmail.com>
2018-11-30 03:42:25 +0000

    Done

--- q11_feature_selection_q02_best_k_features/build.py
+++ q11_feature_selection_q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q11_feature_selection_q02_best_k_features/build.py
 # Default imports
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
@@ -12,15 +13,26 @@ import pandas as pd
 np.random.seed(9)
 
 df = load_data('data/student-mat.csv')
-
 x_train, x_test, y_train, y_test =  split_dataset(df)
-
 x_train,x_test = label_encode(x_train,x_test)
-
-
 np.random.seed(9)
-# Write your code below
+
+def percentile_k_features(x_train, y_train, k=50):
     
+    model = SelectPercentile(f_regression, percentile=k)
+    model.fit(x_train, y_train)
+    cols_list = model.get_support(indices=True)
+    cols_sort = [cols_list for _, cols_list in sorted(zip(model.scores_[cols_list],cols_list), reverse=True)]
+    top_k_predictors = x_train.iloc[:,cols_sort]
+    
+    return list(top_k_predictors.columns.values)
+
+percentile_k_features(x_train, y_train, k=50)
+
+    
+
+
+
 
 
 
