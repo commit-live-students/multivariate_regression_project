088217084085f20c7cdff3db8222695859fa1940
Tusharshah2006 <tusharshah2006@gmail.com>
2018-12-04 17:32:56 +0000

    Done

--- q09_advanced_model_q01_lasso/build.py
+++ q09_advanced_model_q01_lasso/build.py
@@ -1,3 +1,4 @@
+# %load q09_advanced_model_q01_lasso/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
@@ -19,5 +20,16 @@ x_train, x_test, y_train, y_test =  split_dataset(df)
 x_train,x_test = label_encode(x_train,x_test)
 
 # Write your solution here
-
+def lasso(x_train, x_test, y_train, y_test, alpha=0.1):
+    G = Lasso(alpha = alpha)
+    G.fit(x_train, y_train)
+    val = cross_validation_regressor(G,x_train,y_train)
+    y_pred, mse, mae, r2 = regression_predictor(G, x_test, y_test)
     
+    stats = pd.DataFrame([(val,mae,r2,np.sqrt(mse))], columns = ['cross_val','mae','r2','rmse'])
+
+    return G, y_pred, stats
+    
+lasso(x_train, x_test, y_train, y_test)
+
+
