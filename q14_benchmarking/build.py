59689b7e34d930cc8398b88a5174f7b9d0168e56
Tusharshah2006 <tusharshah2006@gmail.com>
2018-12-06 18:46:20 +0000

    Done

--- q14_benchmarking/build.py
+++ q14_benchmarking/build.py
@@ -1,3 +1,4 @@
+# %load q14_benchmarking/build.py
 from greyatomlib.multivariate_regression_project.q01_load_data.build import load_data
 from greyatomlib.multivariate_regression_project.q02_data_split.build import split_dataset
 
@@ -26,5 +27,22 @@ x_train,x_test = label_encode(x_train,x_test)
 
 
 # Write your code below
+def create_stats(x_train, x_test, y_train, y_test):
+    lasso_modl, lasso_y_pred, lasso_stat = lasso(x_train, x_test, y_train, y_test,0.1)
+    ridge_modl, ridge_y_pred, ridge_stat = ridge(x_train, x_test, y_train, y_test,0.1)
+    features = feature_selection(x_train, y_train, k=50)
+    
+    x_train_ft = x_train[features].copy()
+    x_test_ft = x_test[features].copy()
+    
+    lasso_modl_ft, lasso_y_pred_ft, lasso_stat_ft = lasso(x_train_ft, x_test_ft, y_train, y_test,0.1)
+    ridge_modl_ft, ridge_y_pred_ft, ridge_stat_ft = ridge(x_train_ft, x_test_ft, y_train, y_test,0.1)
+    features = feature_selection(x_train_ft, y_train, k=50)
+    
+    complete_stats = pd.concat([lasso_stat,lasso_stat_ft,ridge_stat,ridge_stat_ft])
+    
+    return complete_stats
+
+create_stats(x_train, x_test, y_train, y_test)
 
 
